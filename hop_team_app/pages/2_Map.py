0eec63df36abe63fede3c1bda220b72a53bd0873
Abigail Ezell <maezell@gmail.com>
2026-03-29 18:04:29 -0500

    adding all option

--- hop_team_app/pages/2_Map.py
+++ hop_team_app/pages/2_Map.py
@@ -10,24 +10,39 @@ referral_data = utils.hop_team_nashville_df
 
 with st.sidebar:
     # Create hospital dropdown
-    selected_org = st.selectbox("Select a Hospital", sorted(referral_data['owning_entity'].unique()) )
+    hospital_list = ['--All Hospitals--'] + sorted(referral_data['owning_entity'].unique())
+    selected_org = st.selectbox(
+        'Select a Hospital',
+        options=hospital_list
+    )
     
-    # Get the list of community IDs associated with that hospital 
-    communities = referral_data[referral_data['owning_entity'] == selected_org]['hospital_community'].unique()
-    
+    # Get the list of community IDs associated with that hospital
+    if selected_org == '--All Hospitals--':
+        filtered_data = referral_data
+    else:
+        filtered_data = referral_data[referral_data['owning_entity'] == selected_org]
+        
     #Create dependent dropdown for community selection 
+    community_list = ['--All Communities--'] + sorted(filtered_data['hospital_community'].unique())
     selected_community = st.selectbox(
         f'Select a Referral Community for {selected_org}',
-        communities
+        options=community_list
     )
     
     st.write(f"You selected commmunity #{selected_community} for {selected_org}.")
 
-filtered_df = referral_data[referral_data['hospital_community'] == selected_community].groupby(['organization_name','owning_entity','latitude','longitude','color']).agg({'transaction_count':'sum'}).reset_index()
-filtered_df['transaction_count'] = filtered_df['transaction_count'].apply(lambda x: "{:,}".format(x))
+    if selected_community == '--All Communities--':
+        final_data = referral_data[referral_data['hospital_community'].isin(filtered_data['hospital_community'].unique())]
+    else:
+        final_data = referral_data[referral_data['hospital_community'] == selected_community]
 
-m = folium.Map(location=[filtered_df['latitude'].mean(), filtered_df['longitude'].mean()], zoom_start=8)
-for _, row in filtered_df.iterrows():
+# group the selected community by hospital and calculate total transaction count
+map_df = final_data.groupby(['organization_name','owning_entity','latitude','longitude','color']).agg({'transaction_count':'sum'}).reset_index()
+# add commas to transaction count numbers
+map_df['transaction_count'] = map_df['transaction_count'].apply(lambda x: "{:,}".format(x))
+
+m = folium.Map(location=[map_df['latitude'].mean(), map_df['longitude'].mean()], zoom_start=8)
+for _, row in map_df.iterrows():
     iframe = folium.IFrame('<b>Hospital: </b>' + row['organization_name'] + '<br>' + '<b>Part of: </b>' + row['owning_entity'] + '<br>' + '<b>Total Referrals: </b>' + str(row['transaction_count']))
     popup = folium.Popup(iframe, min_width=300, max_width=400, min_height=100, max_height=100)
     folium.Marker(location=[row['latitude'], row['longitude']], icon=folium.Icon(color=row['color'], icon='map-marker'), popup=popup).add_to(m)
